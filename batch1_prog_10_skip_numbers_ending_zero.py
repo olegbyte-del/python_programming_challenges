be5070b16cf20caf3405a7645e6189ec88c8d4db
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 18:32:21 +0800

    Program that prints all the numbers from 0 to 100 except numbers ending in zero at the same time using list to store values and seperating them with end function

---
+++ batch1_prog_10_skip_numbers_ending_zero.py
@@ -0,0 +1,8 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+numbers = []
+
+for i in range(101):
+    if i % 10 != 0:
+        numbers.append(i)
+        print([i], end=' ')
