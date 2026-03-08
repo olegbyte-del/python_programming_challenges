0c60619099197ccb977b82748b243f0feb37c8ac
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 19:11:42 +0800

    Change print function 2 to 3

--- batch3_prog03_unique_duplicate_checker.py
+++ batch3_prog03_unique_duplicate_checker.py
@@ -1,6 +1,6 @@
 #  Create a program that asks the user to input numbers continuously until an invalid input is entered, and displays “Unique” if the number has no duplicate and “Duplicate” if the number has already been entered.
 
-print("="*50, "Program 2".center(50), "="*50, sep="\n")
+print("="*50, "Program 3".center(50), "="*50, sep="\n")
 print("asks the user to input numbers continuously until an invalid input is entered", 
         "and displays “Unique” if the number has no duplicate and “Duplicate” if the number",
         "has already been entered.", "="*50, sep='\n')
