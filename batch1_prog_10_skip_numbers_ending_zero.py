f869e7d916fb6b7e9d377db9f4783f7fa718ab19
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 18:41:55 +0800

    Added a terminal user interface that identifies what the program number and its purpose

--- batch1_prog_10_skip_numbers_ending_zero.py
+++ batch1_prog_10_skip_numbers_ending_zero.py
@@ -1,5 +1,9 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
+print("="*50, "Program 1".center(50), "="*50, sep="\n")
+print("prints all the numbers from 0 to 100", 
+        "except numbers that ends in zero.", "="*50, sep='\n')
+
 numbers = []
 
 for i in range(101):
