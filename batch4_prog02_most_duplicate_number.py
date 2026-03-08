22c61c616a6aacd4f9716d9b4663811dd58840ed
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 19:44:44 +0800

    Added comments to fix the logic of each variable and use try and except instead of if and elif to prevent the program from crashing if the user inputs a wrong value

--- batch4_prog02_most_duplicate_number.py
+++ batch4_prog02_most_duplicate_number.py
@@ -6,22 +6,24 @@ print("Create a program that asks the user to input numbers continuously",
         "number with the most duplicates (the number that appears most frequently).", "="*50, sep='\n')
 
 number_list = []
+most_frequent = None
+highest_count = 0
+
 while True:
-    most_frequent = None
-    highest_count = 0
+    try:
+        number = int(input("Please input a number: "))
+        
+        number_list.append(number)
+        print("="*50)
+        print(f"Current list: {number_list}".center(50))
+        print("="*50)
+        
+        current_num_count = number_list.count(number) # Stores the number of duplicates
+        
+        if current_num_count > highest_count: # compares the number of duplicate to the highest_count
+            highest_count = current_num_count # if its greater it replaces the value of the highest count
+            most_frequent = number # the number that was recently input is saved to be the most_frequent
     
-    number = int(input("Please input a number: "))
-    
-    number_list.append(number)
-    print("="*50)
-    print(f"Current list: {number_list}".center(50))
-    print("="*50)
-    
-    current_num_count = number_list.count(number)
-    
-    if current_num_count > highest_count:
-        highest_count = current_num_count
-        most_frequent = number
-        print(f"Most Duplicated number: {most_frequent} (Appeared {highest_count} times)", "="*50, sep="\n")
-    else:
+    except ValueError:
         print("="*50, "Invalid input!", "="*50, sep="\n")
+        print(f"Most Duplicated number: {most_frequent} (Appeared {highest_count} times)", "="*50, sep="\n")
