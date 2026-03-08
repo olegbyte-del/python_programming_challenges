80365dea435bd2edfabd3a215cbafc0841d150d0
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 20:57:03 +0800

    allows the user to input the main string and the subtring that is needed to find

---
+++ batch7_prog10_rindex_manual.py
@@ -0,0 +1,10 @@
+# Create a program that asks the user to input a string and a substring, then prints the last occurrence index of the substring in the string, implementing the functionality of rindex() manually without using the built-in function.
+
+print("="*50, "Program 6".center(50), "="*50, sep="\n")
+print("Create a program that asks the user to input a string and a substring", 
+        "then prints the last occurrence index of the substring in the string",
+        "implementing the functionality of rindex() manually without using the built-in function.", "="*50, sep='\n')
+
+main_string = input("Enter the main string: ")
+sub_string = input("Enter the substring to find: ")
+
