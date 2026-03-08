b8093f7a26bb0777c57cd186432a17741799de28
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 19:11:12 +0800

    Created a new program for batch 4 program 2

---
+++ batch4_prog02_most_duplicate_number.py
@@ -0,0 +1,2 @@
+# Create a program that asks the user to input numbers continuously until an invalid input is entered, and displays the number with the most duplicates (the number that appears most frequently).
+
