034a62ec275b6bbd3a34846ed3d3777680bbd733
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 20:45:23 +0800

    User can input a string and each string is seperated and listed at the same time using split and use for each value in list is formatted where first letter is capitalize and the following is lowered case and appended he the new list

--- batch6_prog10_title_case_manual.py
+++ batch6_prog10_title_case_manual.py
@@ -1,1 +1,16 @@
 # Create a program that asks the user to input a string and prints the input in title case manually, capitalizing the first letter of each word without using the built-in title() function.
+
+print("="*50, "Program 6".center(50), "="*50, sep="\n")
+print("program that asks the user to input a string and prints the input in", 
+        "title case manually capitalizing the first letter of each word without",
+        "husing the built-in title() function..", "="*50, sep='\n')
+
+user_input = input("Enter a string: ")
+
+word_list = user_input.split()
+
+formatted_words = []
+
+for word in word_list:
+    format = word[0].upper() + word[1:].lower() 
+    formatted_words.append(format)
