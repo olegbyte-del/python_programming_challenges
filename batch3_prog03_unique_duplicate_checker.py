8a849858b3d145f43608b092fe67cdb4f76baccb
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 19:05:59 +0800

    Added a list and use whle true to run infinite loops and ask the user for a number and the program will check if its not found in the list it will add however if its in the list it will say duplicate

--- batch3_prog03_unique_duplicate_checker.py
+++ batch3_prog03_unique_duplicate_checker.py
@@ -1,1 +1,16 @@
 #  Create a program that asks the user to input numbers continuously until an invalid input is entered, and displays “Unique” if the number has no duplicate and “Duplicate” if the number has already been entered.
+
+print("="*50, "Program 2".center(50), "="*50, sep="\n")
+print("asks the user to input numbers continuously until an invalid input is entered", 
+        "and displays “Unique” if the number has no duplicate and “Duplicate” if the number",
+        "has already been entered.", "="*50, sep='\n')
+
+number_list = []
+while True:
+    number = int(input("Please choose a number: "))
+    
+    if number in number_list:
+        print("Duplicate... Please input a unique number")    
+    else:
+        number_list.append(number)
+        print(f"Unique! Current list: {number_list}")
