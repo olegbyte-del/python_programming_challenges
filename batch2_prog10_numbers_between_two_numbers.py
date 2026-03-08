83b0d486398e470b6fc4c5f2bd078609ca3e4290
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 18:42:41 +0800

    ask the user to input two numbers and prints all numbers between the two inputs and also added a terminal user interface to add personality to the program

--- batch2_prog10_numbers_between_two_numbers.py
+++ batch2_prog10_numbers_between_two_numbers.py
@@ -1,2 +1,11 @@
 # Create a program that asks the user to input two numbers and then prints all numbers between the two inputs.
 
+print("="*50, "Program 2".center(50), "="*50, sep="\n")
+print("asks the user to input two numbers", 
+        "and then prints all numbers between the two inputs.", "="*50, sep='\n')
+
+num1 = int(input("Please choose a number: "))
+num2 = int(input("Please choose a number: "))
+
+for i in range(num1 + 1, num2):
+    print([i], end=" ")
