cc43293650261431b0dbed420c05d5a7ceb55fe8
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-08 19:36:05 +0800

    Added the where user adds a number and appends it on the list at te same time the list is being check in how many duplicates are there and if it fails it shows the number with the most_frequent number appeared on the list

--- batch4_prog02_most_duplicate_number.py
+++ batch4_prog02_most_duplicate_number.py
@@ -1,2 +1,27 @@
 # Create a program that asks the user to input numbers continuously until an invalid input is entered, and displays the number with the most duplicates (the number that appears most frequently).
 
+print("="*50, "Program 4".center(50), "="*50, sep="\n")
+print("Create a program that asks the user to input numbers continuously", 
+        "until an invalid input is entered and displays the",
+        "number with the most duplicates (the number that appears most frequently).", "="*50, sep='\n')
+
+number_list = []
+while True:
+    most_frequent = None
+    highest_count = 0
+    
+    number = int(input("Please input a number: "))
+    
+    number_list.append(number)
+    print("="*50)
+    print(f"Current list: {number_list}".center(50))
+    print("="*50)
+    
+    current_num_count = number_list.count(number)
+    
+    if current_num_count > highest_count:
+        highest_count = current_num_count
+        most_frequent = number
+        print(f"Most Duplicated number: {most_frequent} (Appeared {highest_count} times)", "="*50, sep="\n")
+    else:
+        print("="*50, "Invalid input!", "="*50, sep="\n")
