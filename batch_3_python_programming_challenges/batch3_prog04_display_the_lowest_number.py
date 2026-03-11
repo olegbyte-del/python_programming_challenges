05c4b0d701d2693ddbbc84053b58b1d1f08f0a3c
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 19:55:09 +0800

    replace the variable number to user_number

--- batch_3_python_programming_challenges/batch3_prog04_display_the_lowest_number.py
+++ batch_3_python_programming_challenges/batch3_prog04_display_the_lowest_number.py
@@ -6,7 +6,7 @@ lowest_number = None
 for i in range(100):
     try:
         user_number = int(input("Please input a number: "))
-        number = int(number)
+        number = int(user_number)
 
         if lowest_number is None or number < lowest_number:
             lowest_number = number
