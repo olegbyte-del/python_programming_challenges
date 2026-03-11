cc71137030664b285e470ab5b140b14fa384cd56
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:55:25 +0800

    Ask the user to input their name and also included swapcase for the input function

--- batch_5_python_programming_challenges/batch5_prog06_reverse_casing_of_name.py
+++ batch_5_python_programming_challenges/batch5_prog06_reverse_casing_of_name.py
@@ -1,3 +1,5 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 name = input("Please input your name: ").swapcase()
+
+print(name)
