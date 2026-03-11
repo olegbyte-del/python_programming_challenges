a2eaaebeca98909e0681470573bf8674a36320f4
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:44:03 +0800

    ask the user for their name and include .title() in the input function

--- batch_5_python_programming_challenges/batch5_prog05_ask_the_user_name_and_prints_proper_casing.py
+++ batch_5_python_programming_challenges/batch5_prog05_ask_the_user_name_and_prints_proper_casing.py
@@ -1,1 +1,5 @@
 # Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+name = input("Please input your name: ").title()
+
+print(f"Your name is: {name}")
