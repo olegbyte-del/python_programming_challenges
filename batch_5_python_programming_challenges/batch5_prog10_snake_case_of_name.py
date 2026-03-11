6e22c4710d903b8e3a95600f8ca58bc231bbab29
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 21:05:13 +0800

    Ask the user for their name and created a snake case format

--- batch_5_python_programming_challenges/batch5_prog10_snake_case_of_name.py
+++ batch_5_python_programming_challenges/batch5_prog10_snake_case_of_name.py
@@ -1,1 +1,5 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+name = input("Please inpu your name: ").lower()
+
+snake_case_format = name.replace(" ", "_")
