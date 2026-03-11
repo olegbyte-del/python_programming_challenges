5267469fead870b99637da0fb15266db0a634487
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 19:57:35 +0800

    Ask the user to input a number and appends the number in the list at the same time sorts the list

--- batch_3_python_programming_challenges/batch3_prog05_display_number_from_highest_to_lowest.py
+++ batch_3_python_programming_challenges/batch3_prog05_display_number_from_highest_to_lowest.py
@@ -1,1 +1,12 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+number_list = []
+
+for i in range(100):
+    number = int(input("Please input a number: "))
+    number_list.append(number)
+    
+    number_list.sort()
+
+for num in number_list:
+    print([num], end=" ")
