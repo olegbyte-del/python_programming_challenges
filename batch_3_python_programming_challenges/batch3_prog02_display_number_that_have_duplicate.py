038d24dd27fe81b2013a982ebb023dea46ce9c58
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 19:22:36 +0800

    for every number in list it appends a new number if its the same it skips it

--- batch_3_python_programming_challenges/batch3_prog02_display_number_that_have_duplicate.py
+++ batch_3_python_programming_challenges/batch3_prog02_display_number_that_have_duplicate.py
@@ -1,8 +1,14 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 number_list = []
+unique_list = []
 
 for i in range(10):
     number = int(input("Please input a number: "))
-    
     number_list.append(number)
+    
+for num in number_list:
+    if num not in unique_list:
+        unique_list.append(num)
+        
+print(unique_list)
