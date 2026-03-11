c43c25faf47cb2a6c0c8cc93ca46147bba17fcdd
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:54:06 +0800

    created file for batch 5 program 6-10 excluding 9

---
+++ batch_5_python_programming_challenges/batch5_prog06_reverse_casing_of_name.py
@@ -0,0 +1,3 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+name = input("Please input your name: ").swapcase()
