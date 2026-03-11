dac2ad3baf8074feff0671337c49f4c860f6cd73
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 12:24:53 +0800

    Ask users to input 2 numbers

---
+++ batch_1_python_programming_challenges/batch1_prog_2_compare_numbers_print_equal.py
@@ -0,0 +1,5 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+num1 = int(input("Please input a number: "))
+num2 = int(input("Please input a number: "))
+
