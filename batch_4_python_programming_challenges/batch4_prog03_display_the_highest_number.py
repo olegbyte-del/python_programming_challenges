8636afa2eadf1bf2f4e5d847fa970ad23a6acfc9
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:12:17 +0800

    Ask the user to input a number

--- batch_4_python_programming_challenges/batch4_prog03_display_the_highest_number.py
+++ batch_4_python_programming_challenges/batch4_prog03_display_the_highest_number.py
@@ -1,1 +1,8 @@
-# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest 
+
+highest_number = None
+
+for i in range():
+    number = int(input("Please input a number: "))
+    
+    
