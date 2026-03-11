02c27839bc73140e434e80473ddc73af8a54caa4
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 19:53:58 +0800

    Ask the user to input a number until it is invalid

--- batch_3_python_programming_challenges/batch3_prog04_display_the_lowest_number.py
+++ batch_3_python_programming_challenges/batch3_prog04_display_the_lowest_number.py
@@ -1,1 +1,15 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+
+lowest_number = None
+
+for i in range(100):
+    try:
+        user_number = int(input("Please input a number: "))
+        number = int(number)
+
+    except ValueError:
+        print("Invalid input!")
+        break
+    
+print(f"the lowest number entered was: {lowest_number}")
