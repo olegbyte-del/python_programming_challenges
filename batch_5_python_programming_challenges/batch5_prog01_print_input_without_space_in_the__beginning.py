ba21bf93365bcf3a1cbc47e203c28f3aad9d6531
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:34:55 +0800

    Ask the user for its anme and use .lstrip on the input function and print the name

--- batch_5_python_programming_challenges/batch5_prog01_print_input_without_space_in_the__beginning.py
+++ batch_5_python_programming_challenges/batch5_prog01_print_input_without_space_in_the__beginning.py
@@ -1,1 +1,6 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+name = input("Please enter your fullname: ").lstrip()
+
+print(f"Your name is: {name}")
+
