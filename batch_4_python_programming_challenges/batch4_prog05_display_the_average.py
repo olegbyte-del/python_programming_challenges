4473577e4613d13bcf8d5e402d4403439beaf7ca
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:24:09 +0800

    Ask user to input a number and add it on the list if the user fails to input the irght value it will lead to the average formula and print the average of the list

--- batch_4_python_programming_challenges/batch4_prog05_display_the_average.py
+++ batch_4_python_programming_challenges/batch4_prog05_display_the_average.py
@@ -1,1 +1,12 @@
-# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the averag
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average
+
+number_list = []
+
+while True:
+    try:
+        number = int(input("Please input a number: "))
+        number_list.append(number)
+    except ValueError:
+        average = sum(number_list) / len(number_list)
+        print(number_list, end=" ")
+        break
