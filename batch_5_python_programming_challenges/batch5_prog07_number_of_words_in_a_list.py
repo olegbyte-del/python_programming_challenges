df3d4ecb9e15368a7b49007f84073e54c6ca2871
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:58:35 +0800

    Split the input into list and count the number of items on the list using len and print

--- batch_5_python_programming_challenges/batch5_prog07_number_of_words_in_a_list.py
+++ batch_5_python_programming_challenges/batch5_prog07_number_of_words_in_a_list.py
@@ -1,1 +1,10 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+statement = input("Please input a statement: ")
+
+word_list = statement.split()
+word_count = len(word_list)
+print(f"The number of words in the input is: {word_count}")
+
+
+
