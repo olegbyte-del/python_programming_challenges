6f58b43c48e44885b44222499cdc588dfe262e03
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 19:17:28 +0800

    Ask the user to input 10 numbers and stores in a list

--- batch_3_python_programming_challenges/batch3_prog02_display_number_that_have_duplicate.py
+++ batch_3_python_programming_challenges/batch3_prog02_display_number_that_have_duplicate.py
@@ -1,1 +1,8 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+number_list = []
+
+for i in range(10):
+    number = int(input("Please input a number: "))
+    
+    number_list.append(number)
