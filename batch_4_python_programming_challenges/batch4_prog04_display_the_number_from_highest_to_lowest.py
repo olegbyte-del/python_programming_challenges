f56b33158ba6e130e7020b4e6ccc4cc0c437e881
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:20:16 +0800

    Ask the user to input a number infinitely using while true and appends the number in the list and use sort by reverse =True if user input wrongs it prints the list

--- batch_4_python_programming_challenges/batch4_prog04_display_the_number_from_highest_to_lowest.py
+++ batch_4_python_programming_challenges/batch4_prog04_display_the_number_from_highest_to_lowest.py
@@ -1,1 +1,14 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+number_list = []
+
+while True:
+    try:
+        number = int(input("Please input a number: "))
+        number_list.append(number)
+        
+        number_list.sort(reverse=True)
+    except ValueError:
+        break
+
+print(number_list, end=" ")
