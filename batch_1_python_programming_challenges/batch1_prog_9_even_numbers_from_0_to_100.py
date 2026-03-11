68fae00ac77f8adc9f37440ab3bacd86e5bd7f8c
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 13:08:08 +0800

    Added for loop which prints only even numbers wihtin 0-100 in two ways

--- batch_1_python_programming_challenges/batch1_prog_9_even_numbers_from_0_to_100.py
+++ batch_1_python_programming_challenges/batch1_prog_9_even_numbers_from_0_to_100.py
@@ -1,2 +1,12 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
+
+# for i in range(0, 101, 2):
+#     print([i], end=" ")
+    
+
+# or
+
+for i in range(101):
+    if i % 2 == 0:
+        print([i], end=" ")
