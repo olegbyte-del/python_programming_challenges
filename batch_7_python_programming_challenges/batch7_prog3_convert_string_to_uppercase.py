6e664c7f3e4de117fe9a62330c05ac1434266a04
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 22:31:33 +0800

    List of uppercase letetrs and lowercase letters and ask the user for an input

---
+++ batch_7_python_programming_challenges/batch7_prog3_convert_string_to_uppercase.py
@@ -0,0 +1,10 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+list_of_characters = []
+
+word = input("Enter text: ")
+
+upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+lower = list("abcdefghijklmnopqrstuvwxyz")
+
+
