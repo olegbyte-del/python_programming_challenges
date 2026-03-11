b05c78069c2105df988543a3c45eca8d903c0516
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:10:42 +0800

    for each num it is check by sing count that is coutned for than 1 time and not seen in deplicate list it stores it and append it in the list and print

--- batch_4_python_programming_challenges/batch4_prog01_display_all_numbers_that_have_duplicate.py
+++ batch_4_python_programming_challenges/batch4_prog01_display_all_numbers_that_have_duplicate.py
@@ -1,4 +1,12 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
+list_number = []
+duplicate_number = []
 for i in range(10):
     number = int(input("Please input a number: "))
+    list_number.append(number)
+
+for num in list_number:
+    if list_number.count(num) > 1 and num not in duplicate_number:
+        duplicate_number.append(num)
+        print([num], end=" ")
