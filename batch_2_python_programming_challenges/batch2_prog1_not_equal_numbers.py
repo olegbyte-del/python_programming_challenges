9712b200a94f332f3e7d7c8cf09625010fb6edb8
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 18:33:01 +0800

    Program compares the two number and identifies which is smaller

--- batch_2_python_programming_challenges/batch2_prog1_not_equal_numbers.py
+++ batch_2_python_programming_challenges/batch2_prog1_not_equal_numbers.py
@@ -1,4 +1,9 @@
-# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+# Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 num1 = int(input("Please input a number: "))
 num2 = int(input("Please input a number: "))
+
+if num1 > num2:
+    print(f"The smaller number is: {num2}")
+else:
+    print(f"The smaller number is: {num1}")
