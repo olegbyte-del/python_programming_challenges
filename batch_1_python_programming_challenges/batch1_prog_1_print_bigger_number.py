9d975a144793f179dc5fb1b1058b1d1cb2b4e00e
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 12:21:57 +0800

    the programs compare the two values and determines which has the higher number

--- batch_1_python_programming_challenges/batch1_prog_1_print_bigger_number.py
+++ batch_1_python_programming_challenges/batch1_prog_1_print_bigger_number.py
@@ -4,3 +4,10 @@
 num1 = int(input("Please input a number: "))
 num2 = int(input("Please input a number: "))
 
+if num1 > num2:
+    print(f"The bigger number is {num1}")
+elif num2 > num1:
+    print(f"The bigger number is {num2}")
+else:
+    print("Invalid Input!")
+
