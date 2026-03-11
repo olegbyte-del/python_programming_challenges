ab9a35c25588814f1552667cbbf2736a11ebdc88
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 20:37:43 +0800

    ask the user to input a number and use 0 for adding 0 at fron and 6 numbers whitch is denoted by d equivalent to integer

--- batch_5_python_programming_challenges/batch5_prog02_print_numbers_in_6_digit_format.py
+++ batch_5_python_programming_challenges/batch5_prog02_print_numbers_in_6_digit_format.py
@@ -1,1 +1,6 @@
 #  Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+num = int(input("Please input a number from (0-1000): "))
+
+print(f"Your number is: {num:06d}")
+
