ada3e15ffdcba52e6cbb32d23781370511c1b113
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 12:57:08 +0800

    Used for in range (10) and utilize try and except in order to prevent the program from crashing and used the sum function to add all numbers that the user input

--- batch_1_python_programming_challenges/batch1_prog_7_sum_of_10_numbers.py
+++ batch_1_python_programming_challenges/batch1_prog_7_sum_of_10_numbers.py
@@ -1,1 +1,15 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+list_numbers = []
+for i in range(10):
+    try:
+        number = int(input("Please input a number: "))
+        list_numbers.append(number)
+    except ValueError:
+        print("Invalid Input. Please enter an integer number.")
+        
+total_sum = sum(list_numbers)
+
+print(f"The sum of all numbers is: {total_sum}")
+    
+    
+        
