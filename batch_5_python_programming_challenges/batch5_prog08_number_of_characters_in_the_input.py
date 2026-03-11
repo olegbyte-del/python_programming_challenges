26c145018aea1b04907f20ebe06088302fe5f389
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 21:02:39 +0800

    Ask the user for their name and counts the charcter by using len and print

--- batch_5_python_programming_challenges/batch5_prog08_number_of_characters_in_the_input.py
+++ batch_5_python_programming_challenges/batch5_prog08_number_of_characters_in_the_input.py
@@ -1,1 +1,7 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+name = input("Please input your fullname: ")
+
+char_count = len(name)
+
+print(f"The number of character is: {char_count}")
