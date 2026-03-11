8fb5b601cf9848f6cfe8855ebabc2af35e0cae94
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 13:02:34 +0800

    added a new variable which counts the number of odd numbers and shows the final number of odd numbers

--- batch_1_python_programming_challenges/batch1_prog_8_identify_odd_numbers.py
+++ batch_1_python_programming_challenges/batch1_prog_8_identify_odd_numbers.py
@@ -1,10 +1,17 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 list_numbers = []
+count_odd_numbers = 0
 for i in range(10):
     try:
         number = int(input("Please input a number: "))
         list_numbers.append(number)
+        
+        if i % 2 == 0:
+            count_odd_numbers += 1
+            
     except ValueError:
         print("Invalid Input. Please enter an integer number.")
-        
+
+print(f"There are {count_odd_numbers} odd numbers")
+
