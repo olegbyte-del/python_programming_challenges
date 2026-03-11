9f4999a0de020dc7e961e1820e6d28039bebc908
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 12:30:52 +0800

    created 3 programs at ones with each file have the source code where the program ask the user to input a number

---
+++ batch_1_python_programming_challenges/batch1_prog_4_product_of_two_numbers.py
@@ -0,0 +1,5 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+num1 = int(input("Please input a number: "))
+num2 = int(input("Please input a number: "))
+
---
+++ batch_1_python_programming_challenges/batch1_prog_5_quotient_of_two_numbers_with_decimal_point.py
@@ -0,0 +1,4 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+num1 = float(input("Please input a number: "))
+num2 = float(input("Please input a number: "))
