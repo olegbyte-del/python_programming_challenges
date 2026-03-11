f759b962fcbc2214cbb5d444e8ed633e415710a3
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-11 12:58:36 +0800

    ASkt he user to input 10 numbers

--- batch_1_python_programming_challenges/batch1_prog_8_identify_odd_numbers.py
+++ batch_1_python_programming_challenges/batch1_prog_8_identify_odd_numbers.py
@@ -1,2 +1,10 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
+list_numbers = []
+for i in range(10):
+    try:
+        number = int(input("Please input a number: "))
+        list_numbers.append(number)
+    except ValueError:
+        print("Invalid Input. Please enter an integer number.")
+        
