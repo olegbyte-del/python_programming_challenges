7637faf40720819cdc1799fe830424116aa3719d
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:02:08 +0800

    Askt he user for the string and what part of the string  that is needed to find

---
+++ batch_7_python_programming_challenges/batch7_prog05_manual_startwith_string.py
@@ -0,0 +1,7 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+string = input("Please input a string: ")
+find_string = input("Please identify what string to find: ")
+
+string_list = find_string
+
