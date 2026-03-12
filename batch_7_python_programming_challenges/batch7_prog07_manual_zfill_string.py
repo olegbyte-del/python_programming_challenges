8a2f6ecffb32df12cbc24a0b224078065324e90b
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 08:30:53 +0800

    Ask the user for a number and add 0 infront of it wihtout using zfill()

---
+++ batch_7_python_programming_challenges/batch7_prog07_manual_zfill_string.py
@@ -0,0 +1,5 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+number = int(input("Please input a number: "))
+
+print(f"{number:06d}")
