51463f9f9abad57e1884af64eb1884e15d5925ce
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:20:05 +0800

    find what index is the part of the string and stops there and it will start where the string stops

--- batch_6_python_programming_challenges/batch6_prog2_manual_removeprefix_string.py
+++ batch_6_python_programming_challenges/batch6_prog2_manual_removeprefix_string.py
@@ -2,3 +2,11 @@
 
 string = input("Please input a string: ")
 find_string = input("Please input the part of the string you want to remove: ")
+
+remove_suffix = len(find_string)
+
+if string[:remove_suffix] == find_string:
+    format_version = string[remove_suffix:]
+
+print(f"Result: {format_version}")
+    
