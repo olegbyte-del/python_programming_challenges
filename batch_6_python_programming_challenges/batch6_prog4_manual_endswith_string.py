33d8054e2a7c724f5db134d920109fe4c71454a0
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:31:11 +0800

    checks the last index of the string and determine the part of the string that is needed to find is the same

--- batch_6_python_programming_challenges/batch6_prog4_manual_endswith_string.py
+++ batch_6_python_programming_challenges/batch6_prog4_manual_endswith_string.py
@@ -3,4 +3,10 @@
 string = input("Please input a string: ")
 find_string = input("Please identify the part of the string you want to check: ")
 
-status = True
+find_string_len = len(find_string)
+
+if string[-find_string_len:] == find_string:
+    status = True
+else:
+    status = False 
+print(f"Result: {status}")
