cff038dc8275da1fe94d2a86c18183c98c46fe90
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:37:59 +0800

    adds spaces at the end of the string

--- batch_6_python_programming_challenges/batch6_prog6_manual_ljust_string.py
+++ batch_6_python_programming_challenges/batch6_prog6_manual_ljust_string.py
@@ -1,4 +1,11 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 string = input("Please input a string: ")
-space = int(input("Please identify the spaces that is needed to add: "))
+width = int(input("Please identify the width: "))
+
+space_needed = width - len(string)
+
+if space_needed > 0: 
+    final_format = string + " " * space_needed
+
+print(f"Result: {final_format}")
