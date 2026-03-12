f6c5e4f46194a0c726dec66d57baa6b3513936c6
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:26:40 +0800

    Ask the user for the string and what part of the string he wants to verify

---
+++ batch_6_python_programming_challenges/batch6_prog4_manual_endswith_string.py
@@ -0,0 +1,6 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+string = input("Please input a string: ")
+find_string = input("Please identify the part of the string you want to check: ")
+
+status = True
