7de7e30c3c9d155c6b4457be75d6f59e10b8da6f
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 08:33:42 +0800

    ASk the user to input a string

---
+++ batch_7_python_programming_challenges/batch7_prog04_manual_islower_string.py
@@ -0,0 +1,5 @@
+# Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+string = input("Please input a string: ")
+
+lower = list("abcdefghijklmnopqrstuvwxyz")
