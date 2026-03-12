9cf70f65334318abffc66ba230876de24c384e1d
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:54:36 +0800

    Ask the user for the string and utilize uppercase and lowercase list

---
+++ batch_6_python_programming_challenges/batch6_prog9_manual_capitalize_string.py
@@ -0,0 +1,6 @@
+# Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+string = input("Please input a string: ")
+
+upper = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+lower = list("abcdefghijklmnopqrstuvwxyz")
