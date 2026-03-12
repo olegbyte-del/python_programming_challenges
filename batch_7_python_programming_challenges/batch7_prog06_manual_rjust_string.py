0a5201268b2bbf90c5d7de9d4b5f81b264d0d2b9
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:09:04 +0800

    Ask the user for the string and the spaces he wants the string to move

---
+++ batch_7_python_programming_challenges/batch7_prog06_manual_rjust_string.py
@@ -0,0 +1,4 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+string = input("Please input a string:")
+space = int(input("Please identify the number of spaces that needs to be adjusted: "))
