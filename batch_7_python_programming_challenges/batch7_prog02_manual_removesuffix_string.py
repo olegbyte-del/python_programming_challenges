8a5b4c4bb30b09c5907d7455f4af330d63d7455e
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 08:55:29 +0800

    identiy the statement and removes the the suffix

--- batch_7_python_programming_challenges/batch7_prog02_manual_removesuffix_string.py
+++ batch_7_python_programming_challenges/batch7_prog02_manual_removesuffix_string.py
@@ -3,11 +3,11 @@
 text_file = input("Please input the text file including the file type: ")
 identify_suffix = input("Please identify the type of file you want to remove: ").lower()
 
-remove_suffix = identify_suffix
+remove_suffix = len(identify_suffix)
 
-for char in text_file:
-    try:
-        if char in remove_suffix:
-            -len(char)
-    except ValueError:
-        print("Error!")
+if text_file[-remove_suffix:] == identify_suffix:
+    format_version = text_file[:-remove_suffix]
+else:
+    format_version = text_file
+    
+print(f"Result: {format_version}")
