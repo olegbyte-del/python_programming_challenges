8ed9ab9f6ca53dc817f31561573048a6379fa095
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-12 10:34:14 +0800

    ask the user for the string and the amount of space that is needed to add

---
+++ batch_6_python_programming_challenges/batch6_prog6_manual_ljust_string.py
@@ -0,0 +1,4 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+string = input("Please input a string: ")
+space = int(input("Please identify the spaces that is needed to add: "))
